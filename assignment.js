b286291e8b18de7a248a559cadc9b8009e1c0e2f
Your Namealhaj-cmd <alhazsarkar5523@gmail.com>
2021-01-22 22:40:33 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,37 @@
+kilometerToMeter
+/*function kilometerTOMeter (Meter){
+    var kilometer = 5;
+    var Meter = kilometer*1000;
+    console.log (Meter);
+    
+}  
+*/
+hotelCost
+/**function hotelCost( depth){
+    var day =0;
+    if (depth<=10){
+        animal =depth*100;
+
+    } 
+    else if (depth<=20) {
+        var firstpart =10*100;
+        var remaining =depth-10;
+        var secendpart =remaining*80;
+        day =firstpart+secendpart;
+        
+
+    }
+    else {
+        var firstpart =10*100
+        var secendpart = 10*80
+        var remaining =depth-20;
+        var Thirdpart =remaining*50;
+        day =firstpart+secendpart+Thirdpart;
+    }
+    return day;
+}
+
+var count=day(30)
+console.log(count);
+/*
+   
